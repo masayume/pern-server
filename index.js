c1b2652cd6b68ec2cf5ee54df1228b574dc32845
masayume <masayume.blog@gmail.com>
2020-03-28 13:36:23 +0100

    pern server start

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+
+// create a todo
+
+// get all todo
+
+// update a todo
+
+// delete a todo
+
+app.listen(5000, () => {
+  console.log("server has been started on port 5000")
+})
